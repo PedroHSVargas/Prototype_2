8898b64bbf9388b88a237c51990ff3837bf7953d
Pedro Vargas <pedroh_vargas@hotmail.com>
2025-10-26 18:12:41 -0300

    VERTICAL MOVES ADDED

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;    
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 20;
     public GameObject projectilePrefab;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,12 +23,15 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         
-        if (transform.position.x>xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
-    private float spawnPosZ = 19;
+    private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
