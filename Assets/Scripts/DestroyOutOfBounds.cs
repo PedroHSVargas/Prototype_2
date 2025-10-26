67ccd5f7ee54c7f79bd8bdc850dbc9f6083aad54
Pedro Vargas <pedroh_vargas@hotmail.com>
2025-10-26 18:28:23 -0300

    Destroy animals at side bound

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
-    private float lowerBound = -10;    
+    private float lowerBound = -10;
+    private float sideBound = 30;      
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +18,14 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        } else if (transform.position.x > sideBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        } else if (transform.position.x < -sideBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
